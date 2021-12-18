8705002a5dab396788b5bf2dc3f35c5259f13262
akshayvdes <vd.akshaykumar@gmail.com>
2021-12-18 05:12:52 +0000

    added 2 file

---
+++ argument.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+name=$1
+designation=$2
+age=$3
+company=$4
+
+echo "name of the employe is $?"
+
+echo "designation of the employe is $2"
+
+echo "age of the employee is $3"
+
+echo "the company name is $4"
+
---
+++ pass.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name="Akshay"
+designation="executive engineer"
+age="28"
+company="Parle"
+echo "Employe name is $name"
+echo "Designation is $designation"
+echo "age is $age"
+echo "employer name is $company"
